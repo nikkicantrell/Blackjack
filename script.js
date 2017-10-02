384356ef069dde81436f70f39274309a93ced683
Nicole Cantrell <nikkicantrell@lawn-128-61-8-242.lawn.gatech.edu>
2017-10-02 01:33:05 -0400

    Added hit functionality

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var playerCards = [];
 var dealerCards = [];
+var cards = {'0':'2h', '1':'3h', '2':'4h', '3':'5h', '4':'6h', '5':'7h',
+'6':'8h', '7':'9h', '8':'10h', '9':'Jh', '10':'Qh', '11':'Kh', '12':'Ah',
+'13':'2d', '14':'3d', '15':'4d', '16':'5d', '17':'6d', '18':'7d', '19':'8d',
+'20':'9d', '21':'10d', '22':'Jd', '23':'Qd', '24':'Kd', '25':'Ad',
+'26':'2s', '27':'3s', '28':'4s', '29':'5s', '30':'6s', '31':'7s', '32':'8s',
+'33':'9s', '34':'10s', '35':'Js', '36':'Qs', '37':'Ks', '38':'As',
+'39':'2c', '40':'3c', '41':'4c', '42':'5c', '43':'6c', '44':'7c', '45':'8c',
+'46':'9c', '47':'10c', '48':'Jc', '49':'Qc', '50':'Kc', '51':'Ac'};
+var cardVal = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+'1':10, 'J':10, 'Q':10, 'K':10, 'A':1};
 function deal() {
-    var cards = {'0':'2h', '1':'3h', '2':'4h', '3':'5h', '4':'6h', '5':'7h',
-    '6':'8h', '7':'9h', '8':'10h', '9':'Jh', '10':'Qh', '11':'Kh', '12':'Ah',
-    '13':'2d', '14':'3d', '15':'4d', '16':'5d', '17':'6d', '18':'7d', '19':'8d',
-    '20':'9d', '21':'10d', '22':'Jd', '23':'Qd', '24':'Kd', '25':'Ad',
-    '26':'2s', '27':'3s', '28':'4s', '29':'5s', '30':'6s', '31':'7s', '32':'8s',
-    '33':'9s', '34':'10s', '35':'Js', '36':'Qs', '37':'Ks', '38':'As',
-    '39':'2c', '40':'3c', '41':'4c', '42':'5c', '43':'6c', '44':'7c', '45':'8c',
-    '46':'9c', '47':'10c', '48':'Jc', '49':'Qc', '50':'Kc', '51':'Ac'};
-    var cardVal = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
-    '1':10, 'J':10, 'Q':10, 'K':10, 'A':1};
     playerCards = [];
     dealerCards = [];
     var dealer = document.getElementById("dealer");
@@ -18,25 +18,41 @@ function deal() {
     dealer.innerHTML = '';
     player.innerHTML = '<p id="total"></p>';
     for (i = 0; i < 4; i++) {
-        var card = document.createElement("img");
-        var number = cards[Math.floor(Math.random()*52)];
+        var cardEl = document.createElement("img");
+        var card = newCard();
         if (i%2 == 0) {
-            playerCards.push(number);
-            card.src = 'cards/' + number + '.png';
-            player.appendChild(card);
+            playerCards.push(card);
+            cardEl.src = 'cards/' + card + '.png';
+            player.appendChild(cardEl);
         } else {
-            dealerCards.push(number);
+            dealerCards.push(card);
             if (dealerCards.length < 2) {
-                card.src = 'cards/down.jpg'
-                card.width = '61';
-                card.height = '80';
+                cardEl.src = 'cards/down.jpg'
+                cardEl.width = '61';
+                cardEl.height = '80';
             } else {
-                card.src = 'cards/' + number + '.png';
+                cardEl.src = 'cards/' + card + '.png';
             }
-            dealer.appendChild(card);
+            dealer.appendChild(cardEl);
         }
     }
+    displayTotal();
+}
+function hit() {
+    var player = document.getElementById("player");
+    var cardEl = document.createElement("img");
+    var card = newCard();
+    playerCards.push(card);
+    cardEl.src = 'cards/' + card + '.png';
+    player.appendChild(cardEl);
+    displayTotal();
+}
+function newCard() {
+    return cards[Math.floor(Math.random()*52)];
+}
+function displayTotal() {
     var totalEl = document.getElementById("total");
+    totalEl.innerHTML = '';
     var total = 0;
     var totalA = 0;
     for(i = 0; i < playerCards.length; i++) {
